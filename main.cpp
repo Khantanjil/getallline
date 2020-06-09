94ea0f8ba3f42f7b1fa3e29a1dff88118ebe6f22
Tanjil <tanjilxd@gmail.com>
2020-06-09 14:59:22 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+int main(int argc, char **argv) {
+    
+    std::string greeting;
+    getline(std::cin, greeting);
+    std::cout << greeting << std::endl;
+}
